eca3d1db64be21b7ab764d78a9b996da519bd9d6
nefimov <n.a.efimov@gmail.com>
2025-05-20 19:43:16 +0200

    add arg_check.c and arg*() functions implementations

---
+++ arg_check.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arg_check.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 19:38:39 by nefimov           #+#    #+#             */
+/*   Updated: 2025/05/20 19:41:22 by nefimov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+// Check arguments
+// Return 0 if OK, 1 if not
+int arg_check(int argc, char *argv[])
+{
+	if (argc < 5 || argc > 6 || arg_check_int(argc, argv))
+		return (arg_perrmsg());
+	return (0);
+}
+
+int	arg_perrmsg(void)
+{
+	printf("Wrong arguments!\n");
+	printf("Your program must take the following arguments:\n");
+	printf("number_of_philosophers time_to_die time_to_eat time_to_sleep\n");
+	printf("[number_of_times_each_philosopher_must_eat]\n");
+	return (1);
+}
+
+int	arg_check_int(int argc, char *argv[])
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	while (++i < argc)
+	{
+		str = argv[i];
+		while (*str)
+		{
+			if (*str < '0' || *str > '9')
+				return (1);
+			str++;
+		}
+	}
+	return (0);
+}
--- philo.h
+++ philo.h
@@ -6,12 +6,37 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 15:32:36 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/20 15:33:02 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/20 19:40:29 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_philo
+{
+	int		n;
+	int		int_n;
+	int		t_num;
+	int		fork[2];
+	long	t_eat;
+	long	t_sleep;
+	long	t_die;
+	long	lst_eat;
+	int		is_die;
+	int		eat_n;
+}	t_philo;
+
+// arg_check.c
+int arg_check(int argc, char *argv[]);
+int	arg_check_int(int argc, char *argv[]);
+int	arg_perrmsg(void);
+
 
 
 #endif
