5204f1ad5fd993951e61c80bc5565d4a9d8e7dc7
nefimov <n.a.efimov@gmail.com>
2025-05-20 19:42:24 +0200

    add arg_check() to main()

--- philo.c
+++ philo.c
@@ -6,7 +6,16 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 15:32:26 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/20 15:32:27 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/20 19:41:17 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+int	main(int argc, char *argv[])
+{
+	if (arg_check(argc, argv))
+		return (1);
+	
+	return (0);
+}
