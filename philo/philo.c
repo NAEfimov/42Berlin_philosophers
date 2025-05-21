1840ef954fbfaf5b97ad80628a0d4ed3bfde3be9
nefimov <n.a.efimov@gmail.com>
2025-05-21 17:22:40 +0200

    Makefile, philo.c: update files

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 15:32:26 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/21 16:27:36 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/21 17:17:27 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 
 int	main(int argc, char *argv[])
 {
+	int				args[ARGS_NUM];
 	pthread_t		*thrs;
 	pthread_mutex_t	*mtxs;
 
-	if (ph_arg_check(argc, argv))
+	if (ph_args_check(argc, argv, args))
 		return (1);
 	// Create mutexes
+	if (ph_mtxs_init(&mtxs, args))
+		return (1);
 	// Create philosophers
 	// Create threads
 	// Start threads
 	// Stop threads
 	// Clear allocations
+	ph_mtxs_destroy(&mtxs, args);
 	return (0);
 }
