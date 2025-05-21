a0a99e12d7eba48dc9a934c3bc7da85572afd51b
nefimov <n.a.efimov@gmail.com>
2025-05-21 20:01:30 +0200

    ph_mutex.c ph_thread.c: fix problem with double pointers

--- philo/ph_mutex.c
+++ philo/ph_mutex.c
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 16:32:01 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/21 17:00:57 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/21 19:30:14 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,32 @@
 int	ph_mtxs_init(pthread_mutex_t **mtxs, int *args)
 {
 	int	i;
+	pthread_mutex_t *mtx;
 	
 	// Allocate memorry for an array of mutexes
-	*mtxs = (pthread_mutex_t*)malloc(args[0] * sizeof(pthread_mutex_t));
-	if (*mtxs == NULL)
+	mtx = (pthread_mutex_t *)malloc(args[ARGS_PH_NUM] * sizeof(pthread_mutex_t));
+	if (mtx == NULL)
 		return (1);
 	// Initialize mutexes
 	i = -1;
-	while (++i < args[0])
-		pthread_mutex_init(mtxs[i], NULL);
-	
+	while (++i < args[ARGS_PH_NUM])
+		pthread_mutex_init(&mtx[i], NULL);
+	*mtxs = mtx;
 	return (0);
 }
 
 int	ph_mtxs_destroy(pthread_mutex_t **mtxs, int *args)
 {
 	int	i;
+	pthread_mutex_t *mtx;
 
+	mtx = *mtxs;
 	i = -1;
 	while (++i < args[0])
 	{
-		if (pthread_mutex_destroy(mtxs[i]))
+		if (pthread_mutex_destroy(&mtx[i]))
 			return (1);
 	}
-	free(*mtxs);
+	free(mtx);
 	return (0);
 }
