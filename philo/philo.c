cca06e98bc97a93acaa06c75c564814f49a9cb49
nefimov <n.a.efimov@gmail.com>
2025-05-21 20:05:12 +0200

    philo.c philo.h: update

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 15:32:26 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/21 17:17:27 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/21 20:04:13 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,23 @@
 int	main(int argc, char *argv[])
 {
 	int				args[ARGS_NUM];
-	pthread_t		*thrs;
 	pthread_mutex_t	*mtxs;
+	// pthread_t		*threads;
+	t_philo			*philo;
 
 	if (ph_args_check(argc, argv, args))
 		return (1);
+	ph_args_print(args);
 	// Create mutexes
 	if (ph_mtxs_init(&mtxs, args))
 		return (1);
 	// Create philosophers
+	if (ph_philo_create(&philo, mtxs, args))
+	{
+		// clean and destroy mtxs
+		return (1);
+	}
+	ph_philo_print_status(philo);
 	// Create threads
 	// Start threads
 	// Stop threads
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 15:32:36 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/21 17:18:14 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/21 19:55:52 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,29 @@
 # include <sys/time.h>
 # include <limits.h>
 
-# define ARGS_NUM	5
+# define ARGS_NUM		5
+
+# define ARGS_PH_NUM	0
+# define ARGS_T_DIE		1
+# define ARGS_T_EAT		2
+# define ARGS_T_SLEEP	3
+# define ARGS_N_EATS	4
+
+# define MS_TO_MKS		1000
 
 typedef struct s_philo
 {
 	int				n;
-	int				int_n;
-	int				t_num;
-	int				fork[2];
-	int				t_eat;
-	int				t_sleep;
-	int				t_die;
-	int				lst_eat;
-	int				eat_n;
+	int				status;
+	int				ph_num;
+	long			t_die;
+	long			t_eat;
+	long			t_sleep;
+	int				n_eats;
+	long			t_start;
+	long			t_leat;
 	int				is_die;
-	pthread_mutex_t	mtx[2];
+	pthread_mutex_t	frk[2];
 }	t_philo;
 
 // ph_arg_check.c
@@ -44,10 +52,17 @@ int		ph_arg_perrmsg(char *errmsg);
 void	ph_args_print(int *args);
 
 // ph_utils.c
-int	ph_simple_itoa(char *str);
+int		ph_simple_itoa(char *str);
 
 // ph_mutex.c
-int	ph_mtxs_init(pthread_mutex_t **mtxs, int *args);
-int	ph_mtxs_destroy(pthread_mutex_t **mtxs, int *args);
+int		ph_mtxs_init(pthread_mutex_t **mtxs, int *args);
+int		ph_mtxs_destroy(pthread_mutex_t **mtxs, int *args);
+
+// ph_philosopher.c
+int		ph_philo_create(t_philo **philo, pthread_mutex_t *mtxs, int *args);
+void	ph_philo_init(t_philo *philo, pthread_mutex_t *mtxs, int *args, int order_num);
+void	ph_philo_print(t_philo *philo);
+void	ph_philo_print_all(t_philo *philo);
+void	ph_philo_print_status(t_philo *philo);
 
 #endif
