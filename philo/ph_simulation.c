ff5af79143edfdb6f69db0f72113402379414b6d
nefimov <n.a.efimov@gmail.com>
2025-05-22 19:33:23 +0200

    Working model with strange timings on visualiser and no final exit on die

--- philo/ph_simulation.c
+++ philo/ph_simulation.c
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 09:07:09 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/22 16:20:33 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/22 18:51:24 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	ph_check_die(t_philo *ph)
 }
 int ph_check_t_sleep(t_philo *ph)
 {
-	if (ph_get_msec() - ph->t_leat + ph->t_sleep > ph->t_die)
+	if (ph->t_eat + ph->t_sleep > ph->t_die)
 	{	
 		ph->is_die = 1;
 		return (1);
@@ -53,12 +53,22 @@ void	ph_action_eat(t_philo *ph)
 {
 	pthread_mutex_lock(ph->frk[0]);
 	printf("%ld %d has taken a fork\n", ph_get_msec() - ph->t_start, ph->n);
+	if (ph->ph_num == 1)
+	{
+		usleep(MS_TO_MKS * ph->t_die);
+		ph->is_die = 1;
+		return ;
+	}
 	pthread_mutex_lock(ph->frk[1]);
-	printf("%ld %d has taken a fork\n", ph_get_msec() - ph->t_start, ph->n);
-	
-	printf("%ld %d is eating\n", ph_get_msec() - ph->t_start, ph->n);
-	ph->n_eats--;
 	ph->t_leat = ph_get_msec();
+	printf("%ld %d has taken a fork\n", ph->t_leat - ph->t_start, ph->n);
+	printf("%ld %d is eating\n", ph->t_leat - ph->t_start, ph->n);
+	ph->n_eats--;
+	if (ph_check_wait_time(ph, ph->t_eat))
+		{
+			usleep(MS_TO_MKS * (ph->t_die - ph_get_msec() + ph->t_leat));
+			return ;
+		}
 	usleep(MS_TO_MKS * ph->t_eat);
 
 	pthread_mutex_unlock(ph->frk[0]);
@@ -67,7 +77,7 @@ void	ph_action_eat(t_philo *ph)
 
 void	ph_action_sleep(t_philo *ph)
 {
-	printf("%ld %d is sleeping\n", ph_get_msec() - ph->t_start, ph->n);
+	printf("%ld %d is sleeping\n", ph->t_leat - ph->t_start + ph->t_eat, ph->n);
 	usleep(MS_TO_MKS * ph->t_sleep);
 }
 
@@ -83,6 +93,8 @@ void ph_proc_status(t_philo *ph)
 				return ;
 			
 			ph_action_eat(ph);
+			if (ph->n_eats == 0 || ph->is_die == 1)
+				return ;
 			
 			if (ph_check_t_sleep(ph))
 			{
@@ -97,7 +109,7 @@ void ph_proc_status(t_philo *ph)
 		}
 		else // Action 3
 		{
-			if (ph->t_sleep > ph->t_eat)
+			if (ph->t_sleep >= ph->t_eat)
 				{
 					ph->status--;
 					return ;
@@ -114,7 +126,7 @@ void ph_proc_status(t_philo *ph)
 	}
 	else // Action 2: wait t_eat and eat
 	{
-		if (ph->t_sleep > ph->t_eat)
+		if (ph->t_sleep >= ph->t_eat)
 		{
 			ph->status--;
 			return ;
@@ -136,12 +148,11 @@ void ph_proc_init(t_philo *ph)
 
 	if (ph->status % 2 == 0) // Action 1: take forks and eat
 	{
-		if (ph->status != ph->ph_num - 1)
+		if (ph->status != ph->ph_num - 1 || ph->ph_num == 1)
 		{
-			if (ph_check_die(ph))
-				return ;
-			
 			ph_action_eat(ph);
+			if (ph->n_eats == 0 || ph->is_die == 1)
+				return ;
 			
 			if (ph_check_t_sleep(ph))
 			{
@@ -186,10 +197,7 @@ void	*ph_simulation(void *philo)
 	t_philo *ph;
 
 	ph = (t_philo*)philo;
-	// Set t_start and t_leat
 	ph_set_start_time(ph);
-	// Start loop
-	// ph_philo_print(ph);
 	ph_proc_init(ph);
 	while (ph->n_eats != 0 && !ph->is_die)
 		ph_proc_status(ph);
